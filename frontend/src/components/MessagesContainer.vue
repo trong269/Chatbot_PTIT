f86ea246c42e8913b826c87693d48990571d8886
Nguyen Tien Trong <trongbg2692004@gmail.com>
2025-02-23 22:46:06 +0700

    front end config

--- frontend/src/components/MessagesContainer.vue
+++ frontend/src/components/MessagesContainer.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import { onUpdated, ref, watch } from 'vue'
+import { onUpdated, ref } from 'vue'
 import { SimpleMessage } from '../models/chat'
 import ChatAnswer from './ChatAnswer.vue'
 import ChatMessage from './ChatMessage.vue'
